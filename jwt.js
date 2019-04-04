bb04567e07e1ed1931a8d0e864cc35653212f3d9
colinskeep <colinjinskeep@gmail.com>
2019-04-03 22:24:25 -0700

    sign options

--- jwt.js
+++ jwt.js
@@ -1,3 +1,6 @@
+const signOptions = {
+  algorithm: 'RS256',
+};
 /**
  * @param {string} token - user token
  * @param {object} data - user data
@@ -5,7 +8,7 @@
 async function resolve(token) {
   try {
     const data = await jwt.verify(token, JSON.parse(process.env.PUBLIC_KEY), signOptions);
-    await lastSeen.update(data.email);
+    //await lastSeen.update(data.email);
     return data;
   } catch (err) {
     // eslint-disable-next-line no-console
