bc1d0a3174b7d2ccbc993e84989139638a9cdb98
colinskeep <colinjinskeep@gmail.com>
2019-04-17 00:10:12 -0700

    convo array

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ io.on('connection', async (socket) => {
     console.log(value);
     console.log(value.conversationId);
     console.log(MongoClient.ObjectId(value.conversationId));
-    const messages = await messageCollection.findOne({_id: MongoClient.ObjectId(value.conversationId)});
+    const messages = await messageCollection.findOne({_id: MongoClient.ObjectId(value.conversationId)}, {$project: {convo: 1}});
     console.log(messages);
     io.to(`${socket.conn.id}`).emit('conversationId', messages);
   })
