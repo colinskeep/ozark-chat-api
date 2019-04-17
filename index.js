2a5f84d5a7140746a5f9f8dd67959d7fa7704222
colinskeep <colinjinskeep@gmail.com>
2019-04-16 21:18:10 -0700

    return array of objects

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ io.on('connection', async (socket) => {
   const user = await jwt.resolve(socket.handshake.query.jwt);
   const name = await registrationCollection.findOne({email: user.email});
   const userId = name._id.toString().trim();
-  const messages = await messageCollection.find({participantIds: userId},{$project: { participantIds: 1, usernames: 1, convo: {$slice: -1}}});
+  const messages = await messageCollection.find({participantIds: userId},{$project: { participantIds: 1, usernames: 1, convo: {$slice: -1}}}).toArray();
   console.log(messages);
   if (messages.length > 0) {
     let objects = [];
